5f1bb2a5bea274f304ada36640f8bbc68fd2cb57
Vaše Ime <peapro@gmail.com>
2025-10-15 18:46:44 +0200

    feat: dodato oduzimanje i deljenje sa proverom za nulu

--- src/main/java/com/pecapro/Sabiranje.java
+++ src/main/java/com/pecapro/Sabiranje.java
@@ -8,7 +8,9 @@ public class Sabiranje {
         
         System.out.println("Izaberi operaciju:");
         System.out.println("1 - Sabiranje");
-        System.out.println("2 - Množenje");
+        System.out.println("2 - Oduzimanje");
+        System.out.println("3 - Množenje");
+        System.out.println("4 - Deljenje");
         System.out.print("Unesi broj: ");
         int izbor = scanner.nextInt();
         
@@ -18,14 +20,25 @@ public class Sabiranje {
         System.out.print("Unesi drugi broj: ");
         double b = scanner.nextDouble();
         
-        if (izbor == 1) {
-            double zbir = a + b;
-            System.out.println("Zbir je: " + zbir);
-        } else if (izbor == 2) {
-            double proizvod = a * b;
-            System.out.println("Proizvod je: " + proizvod);
-        } else {
-            System.out.println("Nepoznat izbor!");
+        switch (izbor) {
+            case 1:
+                System.out.println("Zbir je: " + (a + b));
+                break;
+            case 2:
+                System.out.println("Razlika je: " + (a - b));
+                break;
+            case 3:
+                System.out.println("Proizvod je: " + (a * b));
+                break;
+            case 4:
+                if (b == 0) {
+                    System.out.println("Greška: Ne možeš deliti sa nulom!");
+                } else {
+                    System.out.println("Količnik je: " + (a / b));
+                }
+                break;
+            default:
+                System.out.println("Nepoznat izbor! Unesi broj od 1 do 4.");
         }
         
         scanner.close();
